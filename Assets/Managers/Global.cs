656ecc99166ab7682d305a4c91d10baf121e50a6
guest green <guest green>
2017-09-16 09:45:35 -0700

    Folder organization, music manager update

---
+++ Assets/Managers/Global.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Global : MonoBehaviour {
+
+	public static StoryManager storyManager;
+
+	public static MusicManager musicManager;
+
+	public static SoundManager soundManager;
+
+	public static DialogueManager dialogueManager;
+
+	public static bool isBuild = false;
+}
---
+++ Assets/Managers/SoundManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+    public AudioSource footsteps;
+    public AudioSource ghostTalk;
+    
+    public void Play(AudioSource audio)
+    {
+        audio.Play();
+        
+    }
+}
