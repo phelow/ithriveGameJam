a36786f4b77ac2f0d45632c624663a7a5c446fef
wilsonsun <wilson.sun@hotmail.ca>
2017-09-15 22:59:40 -0700

    Added Manager skeletons

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class MusicManager : MonoBehaviour {
+	private Dictionary<string, AudioClip> trackMap;
+	private AudioSource musiceSource;
+
+	public MusicManager() {
+		// Need to initialize music source here
+		musiceSource = ((GameObject)GameObject.Instatiatle(Resources.Load(""))).GetComponent<AudioSource>();
+		trackMap = new Dictionary<string, AudioClip> ();
+	}
+
+	public void Play(string name) {
+
+	}
+
+	public void Stop() {
+
+	}
+
+	public void Unload() {
+
+	}
+}
