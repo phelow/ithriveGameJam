cda124802c64d9e746c05869e0cd55679700f364
sprhughes <james.lee.gibbs@gmail.com>
2017-09-15 22:31:33 -0700

    Parent interactable object

---
+++ Assets/Interactable.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+    private bool _triggered = false;
+
+    [SerializeField]
+    private SpriteRenderer _renderer;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Player")
+        {
+            _triggered = true;
+            _renderer.color = Color.red;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            _triggered = false;
+            _renderer.color = Color.white;
+        }
+    }
+
+    protected virtual void OnInteraction()
+    {
+        Debug.Log("Base class interactable called");
+    }
+}
