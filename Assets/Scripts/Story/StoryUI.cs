19a675eb53c24b5ac3466eca3e88b76d4403d1e4
Danilo <danilo@gimmefriesgames.com>
2017-09-16 11:48:49 -0700

    Adding Phase UI element. (Will need to be integrated into the StoryManager.)

---
+++ Assets/Scripts/Story/StoryUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoryUI : MonoBehaviour {
+
+    public GameObject phasePanel;
+    public TextMeshProUGUI text;
+
+    private void Awake () {
+        phasePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
