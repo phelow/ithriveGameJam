333d197edefa9f45efede718f5e71f64288aeaa8
Danilo <danilo@gimmefriesgames.com>
2017-09-16 14:56:41 -0700

    The code that Wilson could not push cause he's too weak :P

---
+++ Assets/Scripts/Gameplay/CharaterGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharaterGenerator : MonoBehaviour {
+	public GameObject character;
+	public List<GameObject> characters;
+	public int numsOfCharaters;
+	public float duaration;
+	public float[] charaterStartTime;
+	public Vector3[] charaterPostions;
+	private int tempIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < numsOfCharaters; ++i) {
+			if (Time.fixedTime == charaterStartTime [i]) {
+				GameObject clone = (GameObject)GameObject.Instantiate (character);
+				clone.transform.position = charaterPostions[i];
+				characters.Add (clone);
+				Debug.Log (i);
+			}
+		}
+
+		for (int i = 0; i < characters.Count; ++i) {
+			//Debug.Log (characters.Count);
+			if (Time.fixedTime >= charaterStartTime[i]+duaration) {
+				characters [i].SetActive (false);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/UI/DayNight.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DayNight : MonoBehaviour {
+	public Light sceneLight;
+	public Color color0;
+	public Color corlor1;
+	private enum DayTime {Day, Night};
+	private DayTime currentDayTime;
+
+    //public GameObject sceneLight;
+	void Start()
+	{
+		currentDayTime = DayTime.Day;
+		Button btn = this.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+		
+	void TaskOnClick()
+	{
+
+		if (currentDayTime == DayTime.Day) {
+			// Transform the light into NightTime light
+
+			// Set current daytime to Night
+		} 
+		else {
+			// Transform the light into DayTime light
+
+			// Set current daytime to Day
+		}
+	}
+}
