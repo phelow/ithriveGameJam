147eb909d42365db0247b9b3b31fb8f55c8f8c6a
Danilo <danilo@gimmefriesgames.com>
2017-09-16 08:58:23 -0700

    Adding TimeManager The TimeManager handles the passing of time with each action. Simpy call Global.timeManager.incrementTime(); to make time advance when an action is executed.

--- Assets/Scripts/Boot/Global.cs
+++ Assets/Scripts/Boot/Global.cs
@@ -8,6 +8,7 @@ public class Global : MonoBehaviour {
 	public static MusicManager musicManager;
 	public static SoundManager soundManager;
 	public static DialogueManager dialogueManager;
+    public static TimeManager timeManager;
 
 	public static bool isBuild = false;
 }
