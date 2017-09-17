feea53dd8ef13af52193cf76beec4bb9398cc44c
William Pheloung <wpheloung@gmail.com>
2017-09-16 17:58:25 -0700

    new text effect

--- Assets/Scripts/UI/TypeWriter.cs
+++ Assets/Scripts/UI/TypeWriter.cs
@@ -4,7 +4,8 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TypeWriter : MonoBehaviour {
+public class TypeWriter : MonoBehaviour
+{
     public static TypeWriter s_instance;
     public GameObject dialoguePanel;
     public TextMeshProUGUI textBox;
@@ -16,38 +17,60 @@ public class TypeWriter : MonoBehaviour {
     private bool skip = false;
     private bool _animateTextStarted = false;
 
+    private Color _backgroundColorA;
+    private Color _backgroundColorB;
+
+    private Color _backgroundColor;
+
     private IEnumerator AnimateTextRoutine;
 
+
+    private float _clearOffset;
+
     // Use this for initialization
-    void Awake () {
+    void Awake()
+    {
         s_instance = this;
         AnimateTextRoutine = AnimateText();
         dialoguePanel.SetActive(false);
+
+        StartCoroutine(PingPongBackground());
     }
 
-    private void LateUpdate () {
-        if (playing == true && Input.GetKeyDown(KeyCode.Space)) {
-            if (skip == false) {
+    private void LateUpdate()
+    {
+        if (playing == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            if (skip == false)
+            {
                 Global.dialogueManager.FinishCurrent();
-            } else {
+            }
+            else
+            {
                 Global.dialogueManager.SkipCurrent();
             }
         }
     }
 
-    public bool PlayDialogue (Dialogue dialogue) {
-        if (!playing) {
+    public bool PlayDialogue(Dialogue dialogue)
+    {
+        if (!playing)
+        {
             this.sentences = dialogue._sentences;
 
             return PlayToEnd();
-        } else {
+        }
+        else
+        {
             /* This Should Never Happen! */
             return false;
         }
     }
 
-    public bool PlayToEnd () {
-        if (this.sentences != null) {
+    public bool PlayToEnd()
+    {
+        if (this.sentences != null)
+        {
             playing = true;
             currentPointer = 0;
 
@@ -62,23 +85,72 @@ public class TypeWriter : MonoBehaviour {
             skip = false;
             StartCoroutine(AnimateTextRoutine);
             return true;
-        } else {
+        }
+        else
+        {
             Debug.LogError("TypeWriter started typing with no sentences!");
             return false;
         }
     }
 
-    IEnumerator AnimateText () {
+    private static string ToHex(float redValue, float greenValue, float blueValue, float alpha = 1.0f)
+    {
+        return string.Format("#{0:X2}{1:X2}{2:X2}{3:X2}",
+            (int)(alpha * 255),
+            (int)(redValue * 255),
+            (int)(greenValue * 255),
+            (int)(blueValue * 255));
+    }
+
+    private IEnumerator PingPongBackground()
+    {
+        while (true)
+        {
+            _backgroundColor = Color.Lerp(
+                Color.Lerp(_backgroundColorA, _backgroundColorB, Mathf.PingPong(Time.time, 1.0f)),
+                Color.clear,
+                Mathf.PingPong(Time.time + _clearOffset, 1.0f));
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    IEnumerator AnimateText()
+    {
         _animateTextStarted = true;
-        for (int i = 0; i < (this.sentences[currentPointer]._text.Length + 1); i++) {
-            if (skip) {
+
+        _backgroundColorA = new Color(
+            Random.Range(0.1f, 1.0f),
+            Random.Range(0.1f, 1.0f),
+            Random.Range(0.1f, 1.0f));
+
+        _backgroundColorB = new Color(
+            Random.Range(0.1f, 1.0f),
+            Random.Range(0.1f, 1.0f),
+            Random.Range(0.1f, 1.0f));
+        _clearOffset = Random.Range(0.0f, 1.0f);
+
+        for (int i = 0; i < (this.sentences[currentPointer]._text.Length); i++)
+        {
+            if (skip)
+            {
                 break;
             }
 
-            textBox.text = sentences[currentPointer]._text.Substring(0, i);
-            /* TO DO - Add sound? */
+            string currentSentence = sentences[currentPointer]._text;
 
-            yield return new WaitForSeconds(sentences[currentPointer]._speed);
+            string typedText = currentSentence.Substring(0, i);
+            string lastChar = currentSentence.Substring(i, 1);
+            string trimmedString = currentSentence.Substring(i + 1);
+
+            Color foregroundColor = Color.black;
+
+            string text = string.Format("<{0}>{1}</color><{2}><u>{4}</u></color><{5}>{6}</color>",
+            ToHex(Color.white.r, Color.white.g, Color.white.b), typedText, ToHex(foregroundColor.r, foregroundColor.g, foregroundColor.b), 60, lastChar,
+            ToHex(_backgroundColor.r, _backgroundColor.g, _backgroundColor.b, _backgroundColor.a), trimmedString);
+
+            textBox.text = text;
+            /* TO DO - Add sound? */
+            yield return new WaitForSeconds(sentences[currentPointer]._speed); //Lerp down character size
         }
 
         // If skip == true && dialogue is not done playing
@@ -89,14 +161,18 @@ public class TypeWriter : MonoBehaviour {
         NextText();
     }
 
-    public void NextText () {
+    public void NextText()
+    {
         StopAllCoroutines();
         currentPointer++;
         skip = false;
 
-        if (currentPointer < sentences.Count) {
+        if (currentPointer < sentences.Count)
+        {
             StartCoroutine(AnimateText());
-        } else {
+        }
+        else
+        {
             /* TO DO - Animate this transition */
             dialoguePanel.SetActive(false);
 
@@ -105,8 +181,10 @@ public class TypeWriter : MonoBehaviour {
         }
     }
 
-    public void FinishCurrent () {
-        if (playing) {
+    public void FinishCurrent()
+    {
+        if (playing)
+        {
             skip = true;
         }
     }
