9da82a69a7f7d6d2a707d14ad50be72f106bec5c
William Pheloung <wpheloung@gmail.com>
2017-09-17 17:49:08 -0700

    git puaswd

--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -15,49 +15,28 @@ public class SoundManager : MonoBehaviour
 
     private void Awake()
     {
-        if (instance == null)
-            //if not, set it to this.
-            instance = this;
-        //If instance already exists:
-        else if (instance != this)
-            //Destroy this, this enforces our singleton pattern so there can only be one instance of SoundManager.
-            Destroy(gameObject);
-
-        //Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
-        DontDestroyOnLoad(gameObject);
 
     }
 
     public void PlaySingleSound(AudioClip clip)
     {
-        _soundSource.clip = clip;
-        _soundSource.Play();
     }
 
     public void PlayMusic(AudioClip clip)
     {
-        _musicSource.clip = clip;
-        _musicSource.loop = true;
-        _musicSource.Play();
     }
 
     public void PlayRandomSound(List<AudioClip> L_clip)
     {
-        int random = Random.Range(0, L_clip.Count);
-        _soundSource.clip = L_clip[random];
-        _soundSource.Play();
     }
 
     public void PlayDialog(List<AudioClip> L_clip)
     {
-        int random = Random.Range(0, L_clip.Count);
-        _dialogSource.PlayOneShot(L_clip[random]);
 
     }
 
     public void Stop()
     {
-        _soundSource.Stop();
     }
 
 }
